6695e468a233f27afe31f21bf03707f43713b01a
Ivan Ozerets <ivan.ozerets@gmail.com>
2026-05-23 06:40:50 -0500

    pushing small change to data.py, need to find better interface to wsl

--- Projects/nature_classifier/src/tensorflow/data.py
+++ Projects/nature_classifier/src/tensorflow/data.py
@@ -5,19 +5,22 @@ def get_datasets(processed_dir, batch_size, mean, std, test_dir=None, combine_tr
     """
     Create datasets for training, validation, and test datasets.
     """
+    mean_t = tf.constant(mean, dtype=tf.float32)
+    std_t = tf.constant(std, dtype=tf.float32)
+
     def normalize(image, label):
         image = tf.cast(image, tf.float32) / 255.0
-        image = (image - mean) / std
+        image = (image - mean_t) / std_t
 
         return image, label
     
-    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
         os.path.join(processed_dir, "train"),
         image_size=(224, 224),
         batch_size=batch_size,
     ).map(normalize)
 
-    val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    val_dataset = tf.keras.utils.image_dataset_from_directory(
         os.path.join(processed_dir, "val"),
         image_size=(224, 224),
         batch_size=batch_size,
